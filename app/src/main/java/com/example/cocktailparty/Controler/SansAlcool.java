ab3685a91d30002a8d8de0c565c92481489f2266
DFerreira98 <dems@DESKTOP-UCHLFV4.localdomain>
2020-04-25 19:31:45 +0200

    second api

---
+++ app/src/main/java/com/example/cocktailparty/Controler/SansAlcool.java
@@ -0,0 +1,16 @@
+package com.example.cocktailparty.Controler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocktailparty.R;
+
+public class SansAlcool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sans_alcool);
+    }
+}
---
+++ app/src/main/java/com/example/cocktailparty/Model/SansAlcoolAPI.java
@@ -0,0 +1,4 @@
+package com.example.cocktailparty.Model;
+
+public class SansAlcoolAPI {
+}
